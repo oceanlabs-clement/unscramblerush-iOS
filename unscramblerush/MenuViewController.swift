f63d821ad7965da5a290ec8f811e32d6735a0d92
Clementgann <clement@oceantech.asia>
2024-12-26 20:54:32 +0800

    first commit

---
+++ unscramblerush/MenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuViewController.swift
+//  unscramblerush
+//
+//  Created by Clement Gan on 26/12/2024.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
